66967967c331f8cd09f98e122a99f04e43276da8
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-01-23 20:13:46 -0600

    modified obstacle avoidance

--- examples/obstacle_avoidance.py
+++ examples/obstacle_avoidance.py
@@ -8,12 +8,18 @@ def main():
         if not scan_list:
             continue
 
+        print(scan_list)
         tmp = scan_list[3:7]
         print(tmp)
-        if tmp != [2,2,2,2]:
-            fc.turn_right(speed)
-        else:
+        # if tmp != [2,2,2,2]:
+        #     fc.turn_right(speed)
+        # else:
+        #     fc.forward(speed)
+
+        if all([x == 2  for x in tmp]) == 2:
             fc.forward(speed)
+        else:
+            fc.turn_right(speed)
 
 if __name__ == "__main__":
     try: 
