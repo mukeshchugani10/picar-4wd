53306b6ba8fef015e3b47f351e197fb5dd810710
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-01-26 17:17:28 -0600

    Updated forward viewpoint

--- examples/obstacle_avoidance.py
+++ examples/obstacle_avoidance.py
@@ -10,12 +10,6 @@ def main():
             continue
 
         print(scan_list)
-        tmp = scan_list[3:7]
-        print(tmp)
-        # if tmp != [2,2,2,2]:
-        #     fc.turn_right(speed)
-        # else:
-        #     fc.forward(speed)
 
         if all([x == 2 for x in scan_list[3:7]]):
             fc.forward(speed)
@@ -23,7 +17,7 @@ def main():
             view_point = len(scan_list) // 2
             left_view = scan_list[: view_point]
             right_view = scan_list[view_point:]
-            print(right_view, left_view, view_point)
+            print("Right View", right_view, "Left View ", left_view)
             number_of_obstacles_left_view = view_point - left_view.count(2)
             number_of_obstacles_right_view = view_point - right_view.count(2)
             if number_of_obstacles_left_view > number_of_obstacles_right_view:
@@ -33,15 +27,9 @@ def main():
                 fc.turn_left(speed)
                 print("moving left")
 
-        # if tmp != [2, 2, 2, 2]:
-        #     fc.stop()
-        #     turnDirection = randint(0,1)
-        #     fc.backward(speed)
-        #     fc.time.sleep(1)
-        #     if turnDirection == 1:
-        #         fc.turn_left(speed)
-        #     else:
-        #         fc.turn_right(speed)
+            fc.forward(speed)
+            fc.forward(speed)
+
 
 if __name__ == "__main__":
     try: 
